b4a5463db3e551c8a6cd5e1a24958c42891fc938
Siyi Lin <linsiyi7@Gmail.com>
2022-01-16 23:19:09 +0000

    fix bug & run copilot

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@
                   SpringApplication.run(DemoApplication.class, args);
                   }
                   
-                  @GetMapping("/hello")
+                  @GetMapping("/")
                   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
                   return String.format("Hello %s!", name);
                   }
---
+++ start.sh
@@ -0,0 +1,6 @@
+copilot init --app java-demo                      \
+  --name api                                 \
+  --type 'Load Balanced Web Service'         \
+  --dockerfile './Dockerfile'                \
+  --port 8080                                  \
+  --deploy
